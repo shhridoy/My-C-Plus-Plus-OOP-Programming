aee2bef57202fa43e6b82d46a704a63b8cf35ec9
SH Hridoy <shhridoy.cste@gmail.com>
2017-07-09 20:34:05 +0600

    exception of a class

--- Exception handler.cpp
+++ Exception handler.cpp
@@ -5,8 +5,13 @@ class Exception{
     int id;
     string name;
 public:
-    Exception(int i, string n){id=i; name=n;}
-    void Print(void){cout<<"Error Id: "<<id<<" - "<<name<<endl;}
+    Exception(int i, string n){
+        id = i; 
+        name = n;
+    }
+    void Print(void){
+        cout << "Error Id: " << id << " - " << name << endl;
+    }
 };
 
 void Positive(int a)
@@ -14,7 +19,7 @@ void Positive(int a)
     cout<<a<<endl;
     try
     {
-        if(a<1)
+        if(a < 1)
             throw Exception(a, "Not a positive number");
     }
     catch(Exception ob){
@@ -30,3 +35,10 @@ int main()
 
     return 0;
 }
+
+/* OUTPUT:
+5
+-5
+Error Id: -5 - Not a positive number
+15
+*/
